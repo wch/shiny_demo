b9dfae3b0c9aa1d395303d8ce44368d1a0b74783
Winston Chang <winston@stdout.org>
2018-01-08 16:17:36 -0600

    Various updates

--- cran_explorer/app.R
+++ cran_explorer/app.R
@@ -1,19 +1,34 @@
 library(shiny)
 library(shinythemes)
 library(dplyr)
+library(tidyr)
 library(DT)
+library(purrr)
 library(ggplot2)
 library(ggrepel)
 
-all_data <- readRDS("all.rds")
+source("../utils.R")
+
+all_data <- readRDS("packages.rds")
 all_data <- all_data %>%
   # This package has a way earlier date than all others
   filter(Package != "hpower") %>%
   group_by(Package) %>%
   arrange(desc(date))
 
+all_deps <- readRDS("deps.rds")
+
 date_min <- min(all_data$date)
 date_max <- max(all_data$date)
+
+
+deps_summary <- all_deps %>%
+  group_by(Package, Version, type) %>%
+  summarise(n = n()) %>%
+  ungroup() %>%
+  spread(type, n) %>%
+  replace_na(list(Depends = 0L, Imports = 0L, Suggests = 0L))
+
 
 packages_at_date <- function(target_date) {
   all_data %>%
@@ -24,7 +39,7 @@ packages_at_date <- function(target_date) {
 # Compute by using cumsum
 # Talk about precompute for hours
 # Fairly expensive operation - takes ~7s for n=20
-compute_count_by_date <- function(n = 20) {
+compute_count_by_date <- function(n = 4) {
   dates <- seq(date_min, date_max, length.out = n)
   counts <- vapply(dates,
     function(date) packages_at_date(date) %>% nrow(),
@@ -49,11 +64,19 @@ ui <- navbarPage(theme = shinytheme("paper"),
       "Number of packages:",
       textOutput("info_n_packages", inline = TRUE)
     ),
-    DT::dataTableOutput("table")
+    DT::dataTableOutput("table"),
+    actionButton("refresh", "Refresh data")
   ),
   tabPanel("Package info",
-    textInput("package", "Package Name", placeholder = "ggplot2, Rcpp, shiny, ..."),
-    plotOutput("package_timeline", height = "120px")
+    div(
+      textInput("package", "Package Name", placeholder = "ggplot2, Rcpp, shiny, ..."),
+      style="display:inline-block"
+    ),
+    uiOutput("package_version_selector", style = "display:inline-block"),
+    verbatimTextOutput("package_info"),
+    plotOutput("package_timeline", height = "120px"),
+    plotOutput("package_deps_timeline", height = "120px"),
+    tableOutput("package_versions_table")
   )
 
 )
@@ -82,17 +105,61 @@ server <- function(input, output) {
     all_at_date() %>% nrow()
   })
 
+  observeEvent(input$refresh, {
+    download_crandb()
+    crandb_file_to_df()
+  })
+
+
+  selected_package_data <- reactive({
+    all_data %>% filter(Package == input$package)
+  })
+
+  output$package_version_selector <- renderUI({
+    versions <- selected_package_data()$Version
+    selectInput("package_version", "Version", versions, selectize = FALSE, width = "150px")
+  })
+
+  output$package_info <- renderPrint({
+    dat <- selected_package_data() %>% filter(Version == input$package_version)
+    if (nrow(dat) != 1)
+      return(invisible())
+    str(transpose(dat)[[1]])
+  })
 
   output$package_timeline <- renderPlot({
-    dat <- all_data %>% filter(Package == input$package)
+    dat <- selected_package_data()
+
     ggplot(dat, aes(date)) + geom_point(y=1, color = "red") +
       scale_y_continuous(name = NULL, limits = c(0, 2), breaks = NULL) +
       scale_x_date(name = NULL, date_minor_breaks = "1 month") +
       geom_text_repel(y = 1, color = "grey60", aes(label = Version),
         point.padding = 1, min.segment.length = 0) +
-      ggtitle("Release dates") +
+      ggtitle("Releases") +
       theme_bw()
   })
+
+  output$package_deps_timeline <- renderPlot({
+    dat <- gather(deps_summary, type, n, Depends:Suggests) %>%
+      filter(Package == input$package) %>%
+      left_join(all_data, by = c("Package", "Version")) %>%
+      mutate(type = factor(type, levels = c("Suggests", "Imports", "Depends")))
+
+    ggplot(dat, aes(x = date, y = n, fill = type)) + geom_area()
+
+  })
+
+  output$package_versions_table <- renderTable({
+    selected_package_data() %>%
+      ungroup() %>%
+      left_join(deps_summary, by = c("Package", "Version")) %>%
+      mutate(date = as.character(date)) %>%
+      select(Version, Maintainer, License, date, Depends, Imports, Suggests) %>%
+      arrange(desc(date))
+  }, spacing = "xs", hover = TRUE)
+
+
 }
 
 shinyApp(ui, server)
+
--- utils.R
+++ utils.R
@@ -30,14 +30,14 @@ dowload_crandb <- function() {
 
 
 # Convert gzipped JSON file to data frame. Returns the data frame.
-crandb_file_to_df <- function() {
+process_crandb_file <- function() {
   library(dplyr)
   library(jsonlite)
   library(purrr)
   library(readr)
 
   all_data_json <- read_file("all.json.gz")
-  all_data <- fromJSON(all_data_json)
+  all_data_raw <- fromJSON(all_data_json)
 
   # Returns an atomic vector
   extract_col <- function(x, name) {
@@ -62,16 +62,34 @@ crandb_file_to_df <- function() {
     vecs
   }
 
+  extract_list_col <- function(x, name) {
+    lapply(x, function(obj) {
+      obj[[name]]
+    })
+  }
+
   extract_package_info <- function(dat) {
     tibble(
       Package            = extract_col(dat, "Package"),
+      Type               = extract_col(dat, "Type"),
+      Title              = extract_col(dat, "Title"),
       Version            = extract_col(dat, "Version"),
+      `Authors@R`        = extract_col(dat, "Authors@R"),
       Maintainer         = extract_col(dat, "Maintainer"),
+      Description        = extract_col(dat, "Description"),
       License            = extract_col(dat, "License"),
-      Depends            = extract_dep_col(dat, "Depends"),
-      Suggests           = extract_dep_col(dat, "Suggests"),
-      NeedsCompilation   = recode(extract_col(dat, "NeedsCompilation"), yes = TRUE, no = FALSE, .missing = FALSE),
+      Depends            = extract_list_col(dat, "Depends"),
+      Imports            = extract_list_col(dat, "Imports"),
+      Suggests           = extract_list_col(dat, "Suggests"),
+      NeedsCompilation   = recode(extract_col(dat, "NeedsCompilation"), yes = TRUE, no = FALSE, .missing = FALSE, .default = FALSE),
+      URL                = extract_col(dat, "URL"),
+      BugReports         = extract_col(dat, "BugReports"),
+      Collate            = extract_col(dat, "Collate"),
+      Author             = extract_col(dat, "Author"),
+      Repository         = extract_col(dat, "Repository"),
+      MD5sum             = extract_col(dat, "MD5sum"),
       Packaged           = extract_col(dat, "Packaged"),
+      `Date/Publication` = extract_col(dat, "Date/Publication"),
       # `Date` is supplied by the author, and they often don't update it.
       # Date               = extract_col(dat, "Date"),
       # `date` is the actual date that the package is on CRAN (I think)
@@ -79,12 +97,65 @@ crandb_file_to_df <- function() {
     )
   }
 
-  all_data <- all_data %>% map("versions") %>% flatten() %>% unname()
+  all_data <- all_data_raw %>% map("versions") %>% flatten() %>% unname()
   all_data <- all_data %>% extract_package_info()
 
   # Data cleaning
   all_data <- all_data %>%
     filter(!is.na(Version))
 
-  all_data
+
+  # =================== Create table of dependency data =======================
+  create_dep_list <- function(Package, Version, pkg_deps) {
+    # Remove R dependency
+    pkg_deps[["R"]] <- NULL
+    n_deps <- length(pkg_deps)
+
+    list(
+      Package = rep(Package, n_deps),
+      Version = rep(Version, n_deps),
+      name    = names(pkg_deps),
+      ver     = as.character(pkg_deps)
+    )
+  }
+
+  depends <- mapply(
+    create_dep_list,
+    all_data$Package, all_data$Version, all_data$Depends,
+    SIMPLIFY = FALSE
+  )
+  depends <- depends %>% transpose() %>% map(flatten_chr) %>% as_tibble()
+  depends$type <- "Depends"
+
+  imports <- mapply(
+    create_dep_list,
+    all_data$Package, all_data$Version, all_data$Imports,
+    SIMPLIFY = FALSE
+  )
+  imports <- imports %>% transpose() %>% map(flatten_chr) %>% as_tibble()
+  imports$type <- "Imports"
+
+  suggests <- mapply(
+    create_dep_list,
+    all_data$Package, all_data$Version, all_data$Suggests,
+    SIMPLIFY = FALSE
+  )
+  suggests <- suggests %>% transpose() %>% map(flatten_chr) %>% as_tibble()
+  suggests$type <- "Suggests"
+
+  all_deps <- bind_rows(depends, imports, suggests)
+
+
+  # Remove Depends, Imports, and Suggests columns from all_data since the data
+  # has been extracted into its own table.
+  all_data$Depends <- NULL
+  all_data$Imports <- NULL
+  all_data$Suggests <- NULL
+
+  # =================================================
+  list(
+    # raw = all_data_raw,
+    packages = all_data,
+    deps = all_deps
+  )
 }
