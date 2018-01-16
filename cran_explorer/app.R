f0ab6220b17490380e458e2b37f27018fdcbe2c2
Joe Cheng <joe@rstudio.com>
2018-01-16 14:54:45 -0800

    Cache plots on "Package info" tab

--- cran_explorer/app.R
+++ cran_explorer/app.R
@@ -8,6 +8,7 @@ library(ggplot2)
 library(ggrepel)
 
 source("utils.R")
+source("plot_cache.R")
 
 all_data <- read_csv("packages.csv")
 makeReactiveBinding("all_data")
@@ -70,6 +71,66 @@ deps_summary <- reactive({
 })
 
 
+plot_width <- 800
+plot_height <- 240
+plot_retina <- 2
+
+plot_cache <- plotCache("package_timeline", all_deps,
+  width = plot_width * plot_retina, height = plot_height * plot_retina,
+  res = 72 * plot_retina,
+  function(package, dat) {
+    if (nrow(dat) == 0)
+      return()
+
+    deps <- gather(deps_summary(), type, n, Depends:Suggests) %>%
+      filter(Package == package) %>%
+      left_join(all_data, by = c("Package", "Version")) %>%
+      mutate(type = factor(type, levels = c("Suggests", "Imports", "Depends")))
+
+    y_max <- deps %>%
+      group_by(Package, Version) %>%
+      summarise(n = sum(n)) %>%
+      pull(n) %>%
+      max()
+    y_lims <- c(-0.5*y_max, y_max)
+    y_breaks <- pretty(c(0, y_max), n = 3)
+
+    # Create data set for step plot (we would use geom_step here, but it doesn't
+    # fill area under curve)
+    deps_step <- deps %>%
+      group_by(type) %>%
+      arrange(type, date)
+
+    deps_step <- bind_rows(
+      deps_step,
+      mutate(
+        deps_step,
+        date = date - 0.01,
+        n = lag(n, default = NA)
+      )
+    ) %>%
+      drop_na(n)
+
+    p <- ggplot(deps_step, aes(x = date)) +
+      geom_area(aes(y = n, fill = type), position = "stack", alpha = 0.4) +
+      geom_hline(yintercept = 0, size = 0.4) +
+      geom_line(aes(y = n, group = type), position = "stack", size = .2) +
+      geom_point(data = dat, y = 0, size = 1, color = "red") +
+      geom_text_repel(data = dat, y = 0, angle = 90, color = "grey60",
+        ylim = c(NA, 0), aes(label = Version),
+        point.padding = 1, min.segment.length = 0) +
+      scale_fill_brewer(palette = "Blues") +
+      scale_y_continuous(name = NULL, limits = y_lims, breaks = y_breaks, expand = c(0.05, 0)) +
+      scale_x_date(name = NULL, date_minor_breaks = "1 month", expand = c(0.02, 0)) +
+      guides(fill = guide_legend(title = NULL)) +
+      ggtitle("Releases and dependencies over time") +
+      theme_bw() +
+      theme(legend.position = "bottom")
+    print(p)
+  }
+)
+
+
 # =============================================================================
 # UI
 # =============================================================================
@@ -93,7 +154,7 @@ ui <- navbarPage(theme = shinytheme("paper"),
   "CRAN explorer",
   id = "tabs",
   tabPanel("Overview",
-    tags$head(tags$style(HTML("body { overflow-y: scroll; }"))),
+    tags$head(tags$style(HTML("body { overflow-y: scroll; } #package_timeline { max-width: 100%; }"))),
     uiOutput("date_slider_ui"),
     plotOutput("cran_timeline", height = "160px"),
     div(
@@ -137,7 +198,7 @@ ui <- navbarPage(theme = shinytheme("paper"),
     ),
     uiOutput("package_version_selector", style = "display:inline-block"),
     uiOutput("package_info"),
-    plotOutput("package_timeline", height = "240px"),
+    imageOutput("package_timeline", width = plot_width, height = "auto"),
     tableOutput("package_versions_table")
   )
 )
@@ -279,56 +340,14 @@ server <- function(input, output) {
     )
   })
 
-  output$package_timeline <- renderPlot({
-    dat <- selected_package_data()
-    if (nrow(dat) == 0)
-      return()
-
-    deps <- gather(deps_summary(), type, n, Depends:Suggests) %>%
-      filter(Package == input$package) %>%
-      left_join(all_data, by = c("Package", "Version")) %>%
-      mutate(type = factor(type, levels = c("Suggests", "Imports", "Depends")))
-
-    y_max <- deps %>%
-      group_by(Package, Version) %>%
-      summarise(n = sum(n)) %>%
-      pull(n) %>%
-      max()
-    y_lims <- c(-0.5*y_max, y_max)
-    y_breaks <- pretty(c(0, y_max), n = 3)
-
-  # Create data set for step plot (we would use geom_step here, but it doesn't
-  # fill area under curve)
-  deps_step <- deps %>%
-    group_by(type) %>%
-    arrange(type, date)
-
-  deps_step <- bind_rows(
-      deps_step,
-      mutate(
-        deps_step,
-        date = date - 0.01,
-        n = lag(n, default = NA)
-      )
-    ) %>%
-    drop_na(n)
-
-    ggplot(deps_step, aes(x = date)) +
-      geom_area(aes(y = n, fill = type), position = "stack", alpha = 0.4) +
-      geom_hline(yintercept = 0, size = 0.4) +
-      geom_line(aes(y = n, group = type), position = "stack", size = .2) +
-      geom_point(data = dat, y = 0, size = 1, color = "red") +
-      geom_text_repel(data = dat, y = 0, angle = 90, color = "grey60",
-        ylim = c(NA, 0), aes(label = Version),
-        point.padding = 1, min.segment.length = 0) +
-      scale_fill_brewer(palette = "Blues") +
-      scale_y_continuous(name = NULL, limits = y_lims, breaks = y_breaks, expand = c(0.05, 0)) +
-      scale_x_date(name = NULL, date_minor_breaks = "1 month", expand = c(0.02, 0)) +
-      guides(fill = guide_legend(title = NULL)) +
-      ggtitle("Releases and dependencies over time") +
-      theme_bw() +
-      theme(legend.position = "bottom")
-  })
+  output$package_timeline <- renderImage({
+    path <- plot_cache(input$package, selected_package_data())
+    list(
+      src = path,
+      width = "100%",
+      height = "auto"
+    )
+  }, deleteFile = FALSE)
 
   output$package_versions_table <- renderTable({
     dat <- selected_package_data()
